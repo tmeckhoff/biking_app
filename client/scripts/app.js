f5c28dd3bfabeba4c61d9fae1da908f69a78169e
Tara Newberry <newberrytara30@gmail.com>
2015-09-04 14:57:49 -0500

    data posting successfully

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,28 +13,58 @@ myApp.config(['$routeProvider', function($routeProvider){
         })
 }]);
 
-myApp.controller('firstController', ['$scope', '$http', function($scope, $http) {
+myApp.controller('firstController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
 
-    $scope.rating = {};
     $scope.ratings = [];
 
     $scope.sendRating = function(){
         return $http.post('/rating', {title: $scope.title, rating: $scope.rating, comment: $scope.comment, latitude: $scope.latitude, longitude: $scope.longitude})
-                .then(alert('You added something'));
+                .success(function(response) {
+                $scope.title = "";
+                $scope.rating = "";
+                $scope.comment = "";
+                $scope.successMessage = "You rated it!";
+                $scope.showSuccessMessage = true;
+                $scope.refreshRatings();
+            });
 
     };
 
+    $scope.refreshRatings = function(){
+        return $http.get('/rating').then(function(response){
+            if(response.status !== 200){
+                throw new Error('Failed to get ratings!');
+            }
+            $scope.ratings.push(response.data);
+            return response.data;
 
-    //$scope.viewRatings = function(){
-    //    return $http.get('/rating').then(function(response){
-    //        if(response.status !== 200){
-    //            throw new Error('Failed to get your ratings!')
-    //        }
-    //        $scope.markerRatings = response.data;
-    //        return response.data;
-    //    });
-    //};
-    //viewRatings();
+            });
+    };
+console.log($scope.ratings);
+
+
+    //for (var i = 0, l = response.length; i < l; i++) {
+    //
+    //    $scope.r = response[i];
+    //
+    //
+    //    $scope.contentString = '<div class="info-box"><h5>' +
+    //        r.title +
+    //        ' said:</h5><p>' +
+    //        r.rating +
+    //        '</p><br/><p>' +
+    //        r.comment +
+    //        '</p><br/></div>';
+
+
+        $rootScope.$on('hideMessages', function(){
+        $scope.$apply(function(){
+            $scope.showSuccessMessage = false;
+
+        });
+
+    });
+
 
     //start of map stuff
 
@@ -135,8 +165,6 @@ myApp.controller('firstController', ['$scope', '$http', function($scope, $http) 
     var marker;
 
 
-    var html;//need a get from the server to put info from the form and attach it to that particular marker that was added to the map.
-    //Also need a delete button here to delete the marker from the map and database.
 
 
     google.maps.event.addListener(map, "click", function(event) {
@@ -148,13 +176,13 @@ myApp.controller('firstController', ['$scope', '$http', function($scope, $http) 
             icon: "../assets/styles/images/cycling.png"
         });
 
+        $rootScope.$broadcast("hideMessages");
+
             $scope.latitude = marker.getPosition().lat();
             $scope.longitude = marker.getPosition().lng();
 
-
-
         var infowindow = new google.maps.InfoWindow({
-            content: html
+            content: $scope.contentString
         });
 
         google.maps.event.addListener(marker, "click", function() {
@@ -163,13 +191,14 @@ myApp.controller('firstController', ['$scope', '$http', function($scope, $http) 
         });
 
         google.maps.event.addListener(infowindow, "closeclick", function() {
-            marker.setMap(null);
+           //do delete functionality here or in a button?
 
         });
 
 
 
     });
+
 
 
 
