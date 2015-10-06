03b2f04b7c90c10a2d46487d0f091a657c0bc742
Tara Newberry <newberrytara30@gmail.com>
2015-10-06 12:13:10 -0500

    fixed undefined comment issue

--- server/models/rating.js
+++ server/models/rating.js
@@ -5,7 +5,7 @@ var RatingSchema = new Schema({
     name: {type: String, required: true, unique: false},
     title: {type: String, required: true, unique: false},
     rating: {type: Number, required: true, unique: false},
-    comment: {type: String, required: false, unique: false},
+    comment: {type: String, required: true, unique: false},
     latitude: {type: Number, required: true, unique: false},
     longitude: {type: Number, required: true, unique: false}
 });
